7143806ca34f71f1101ee0c019e7c3f82af7c59f
Alladin Melico <melico.alladin@gmail.com>
2021-03-08 14:53:43 +0800

    algo 3 act 2

--- Algo3.js
+++ Algo3.js
@@ -6,6 +6,16 @@ function printUpTo(x) {
 		console.log(i);
 	}
 }
-printUpTo(1000); // should print all the integers from 1 to 1000
+// printUpTo(1000); // should print all the integers from 1 to 1000
 y = printUpTo(-10); // should return false
 console.log(y); // should print false
+
+function printSum(x) {
+	var sum = 0;
+	for (let i = 0; i <= x; i++) {
+		sum += i;
+	}
+	return sum;
+}
+y = printSum(255); // should print all the integers from 0 to 255 and with each integer print the sum so far.
+console.log(y); // should print 32640
