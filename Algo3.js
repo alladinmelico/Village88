3b0022adaeb9155bac28ea8fc435b93a12ee119b
Alladin Melico <melico.alladin@gmail.com>
2021-03-08 15:01:23 +0800

    finished algo 3

--- Algo3.js
+++ Algo3.js
@@ -1,3 +1,4 @@
+// Print 1 to x
 function printUpTo(x) {
 	if (x <= 0) {
 		return false;
@@ -6,10 +7,11 @@ function printUpTo(x) {
 		console.log(i);
 	}
 }
-// printUpTo(1000); // should print all the integers from 1 to 1000
+printUpTo(1000); // should print all the integers from 1 to 1000
 y = printUpTo(-10); // should return false
 console.log(y); // should print false
 
+// PrintSum
 function printSum(x) {
 	var sum = 0;
 	for (let i = 0; i <= x; i++) {
@@ -18,4 +20,27 @@ function printSum(x) {
 	return sum;
 }
 y = printSum(255); // should print all the integers from 0 to 255 and with each integer print the sum so far.
-console.log(y); // should print 32640
+// console.log(y); // should print 32640
+
+// PrintSumArray
+function printSumArray(x) {
+	var sum = 0;
+	for (var i = 0; i < x.length; i++) {
+		sum += x[i];
+	}
+	return sum;
+}
+console.log(printSumArray([1, 2, 3])); // should log 6
+
+// LargestElement
+function largestElement(arr) {
+	let large = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (large < arr[i]) {
+			large = arr[i];
+		}
+	}
+	return large;
+}
+
+console.log(largestElement([1, 30, 5, 7]));
